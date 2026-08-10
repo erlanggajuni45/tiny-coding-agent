10ebec1c5215acf9a0dd9169571ac54c6db88a4c
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-10 08:45:16 +0700

    connect to OpenRouter API, make request, and show its response

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from tiny-coding-agent!")
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get('OPENROUTER_API_KEY')
+    
+    if api_key is None:
+        raise RuntimeError("api_key isn't configured yet")
+    
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key
+    )
+    
+    response = client.chat.completions.create(model='openrouter/free', messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ])
+    
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
